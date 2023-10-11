db83d4b9fdd997367676bb81701d3a1f2f83157a
NXT1008 <nguyenxuanthe10082003@gmail.com>
2023-10-11 19:38:44 +0700

    he

--- Function.sql
+++ Function.sql
@@ -65,3 +65,58 @@ begin
 	select @tenDangNhap = maNhanVien, @chucVu = chucVu from deleted
 	delete TAIKHOAN where TAIKHOAN.tenDangNhap = @tenDangNhap
 end
+
+-- Tạo TRIGGER khi thêm nhân viên thì tài khoản tự động thêm
+create or alter trigger trg_ThemTaiKhoan
+on NHANVIEN
+for insert
+as
+begin
+	declare @taiKhoan nvarchar(20), @chucVu nvarchar(50)
+	select @taiKhoan = maNhanVien, @chucVu = chucVu from inserted
+	insert into TAIKHOAN values (@taiKhoan, '1', @chucVu)
+end
+go
+
+-- Test
+begin tran
+	INSERT INTO CHINHANH (maChiNhanh, tenChiNhanh, diaChi)
+	VALUES ('CN001', N'Chi nhánh A', N'123 Đường A, Quận 1, TP.HCM')
+	INSERT INTO NHANVIEN (maNhanVien, hoTenNhanVien, CCCD, ngaySinh, gioiTinh, diaChi, soDienThoai, chucVu, maChiNhanh)
+	VALUES ('NV001', N'Nguyễn Văn A', '123456789012', '1990-05-15', N'Nam', N'123 Đường X, Quận Y, TP.HCM', '0123456789', N'Quản lý', 'CN001')
+	select * from TaiKhoan
+rollback
+go
+
+-- Tạo trigger Khi sửa mã nhân viên thì tài khoản cũng sẽ cập nhật theo
+create or alter trigger trg_CapNhatTaiKhoan
+on NHANVIEN
+for update
+as
+begin
+	declare @taiKhoanCu nvarchar(20), @taiKhoanMoi nvarchar(20)
+	set @taiKhoanMoi = (select maNhanVien from inserted)
+	set @taiKhoanCu = (select maNhanVien from deleted)
+	Update TAIKHOAN
+	set tenDangNhap = @taiKhoanMoi
+	where tenDangNhap = @taiKhoanCu
+end
+go
+-- Test
+begin tran
+	INSERT INTO CHINHANH (maChiNhanh, tenChiNhanh, diaChi)
+	VALUES ('CN001', N'Chi nhánh A', N'123 Đường A, Quận 1, TP.HCM')
+	INSERT INTO NHANVIEN (maNhanVien, hoTenNhanVien, CCCD, ngaySinh, gioiTinh, diaChi, soDienThoai, chucVu, maChiNhanh)
+	VALUES ('NV001', N'Nguyễn Văn A', '123456789012', '1990-05-15', N'Nam', N'123 Đường X, Quận Y, TP.HCM', '0123456789', N'Quản lý', 'CN001')
+
+	select * from TaiKhoan
+
+	update NHANVIEN
+	set maNhanVien = 'NV002'
+	where maNhanVien = 'NV001'
+
+	select * from TAIKHOAN
+rollback
+go
+
+--
