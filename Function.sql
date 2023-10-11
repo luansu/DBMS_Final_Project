99ec8d36fa633b2792c74b7a58e9dfa30529538b
danggiathuan@2003 <giathuanhl@gmail.com>
2023-10-11 15:45:10 +0700

    Trigger

--- Function.sql
+++ Function.sql
@@ -33,6 +33,35 @@ GROUP BY pn.maChiNhanh, ctpn.maXe
 select * from v_KhoXeTheoChiNhanh
 
 --Xem danh sach nhan vien co chuc vu la quan ly
+CREATE or alter VIEW v_NhanVienQuanLy as
+select nv.maNhanVien, nv.hoTenNhanVien, nv.soDienThoai, cn.maChiNhanh, cn.tenChiNhanh
+from NHANVIEN as nv, CHINHANH as cn
+where nv.maChiNhanh = cn.maChiNhanh and nv.chucVu = 'Quản lý'
+
+delete NhanVien where maChiNhanh = 'CNHN'
+delete Chinhanh where maChinhanh = 'CNHN'
+
+select * from v_NhanVienQuanLy
+--Xem 
 
 --TRIGGER
+-- Tự động thêm tài khoản khi thêm nhân viên
+create or alter trigger tg_UpdateTK on NhanVien
+for insert
+as
+begin
+	declare @tenDangNhap nvarchar(20), @chucVu nvarchar(50)
+	select @tenDangNhap = maNhanVien, @chucVu = chucVu from inserted
+	insert into TAIKHOAN (tenDangNhap, matKhau, chucVu) values (@tenDangNhap, '1234', @chucVu)
+end
 
+
+-- Tự động xóa tài khoản khi xóa nhân viên
+create or alter trigger tg_DeleteTK on NhanVien
+for delete
+as
+begin
+	declare @tenDangNhap nvarchar(20), @chucVu nvarchar(50)
+	select @tenDangNhap = maNhanVien, @chucVu = chucVu from deleted
+	delete TAIKHOAN where TAIKHOAN.tenDangNhap = @tenDangNhap
+end
