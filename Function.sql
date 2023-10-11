db703dd9f7f9e0cc9c852f54c637a5a422ea7801
luansu <sm.luan2003@gmail.com>
2023-10-12 05:22:17 +0700

    Them 2 trigger

--- Function.sql
+++ Function.sql
@@ -6,22 +6,23 @@ SELECT hotenNhanVien, CCCD, ngaySinh, gioiTinh, soDienThoai
 FROM NHANVIEN as nv
 WHERE nv.tinhTrangLamViec = 1
 go 
+
+select * from v_DSNhanVienConLamViec
+
 --Xem danh sách chi nhánh
 CREATE VIEW v_DSChiNhanh as
 SELECT * 
 FROM CHINHANH
 go
+
+select * from v_DSChiNhanh
 --Xen danh sách nhà cung cấp
 CREATE VIEW v_DSNhaCungCap as
 SELECT *
 FROM NHACUNGCAP
 go
---Xem danh sách Xe
-CREATE VIEW v_DSXeConTrongKho as
-SELECT maXe, tenXe, mauSac, soChoNgoi, xuatXu, hangXe, loaiXe, phienBanXe, pn.soLuong
-FROM XE, PHIEUNHAP as pn
-WHERE pn.soLuong > 0
-go
+
+select * from v_DSNhaCungCap
 
 --View xem số xe đã bán theo chi nhánh
 CREATE or ALTER VIEW v_SoXeDaBan as
@@ -56,22 +57,43 @@ select nv.maNhanVien, nv.hoTenNhanVien, nv.soDienThoai, cn.maChiNhanh, cn.tenChi
 from NHANVIEN as nv, CHINHANH as cn
 where nv.maChiNhanh = cn.maChiNhanh and nv.chucVu = 'Quản lý'
 
-
-
 delete NhanVien where maChiNhanh = 'CNHN'
 delete Chinhanh where maChinhanh = 'CNHN'
 
 select * from v_NhanVienQuanLy
 --Xem danh sach xe co xuat xu Nhat Ban
 create or alter view v_XeXuatXuNhatBan as
-select tenXe, giaBan, soChoNgoi,loaiXe, loaiDongCo, loaiNhienLieu
-from LOXE where xuatXu = 'Nhật Bản'
+select maXe, tenXe, giaBan, soChoNgoi,loaiXe, loaiDongCo, loaiNhienLieu
+from Xe where xuatXu = N'Nhật Bản'
 
 select * from v_XeXuatXuNhatBan
 
-go
+--TRIGGER
+-- Tự động thêm tài khoản khi thêm nhân viên
+create or alter trigger tg_UpdateTK on NhanVien
+for insert
+as
+begin
+	declare @tenDangNhap nvarchar(20), @chucVu nvarchar(50)
+	select @tenDangNhap = maNhanVien, @chucVu = chucVu from inserted
+	insert into TAIKHOAN (tenDangNhap, matKhau, chucVu) values (@tenDangNhap, '1234', @chucVu)
+end
+
+INSERT INTO NHANVIEN (maNhanVien, hoTenNhanVien, CCCD, ngaySinh, gioiTinh, diaChi, soDienThoai, chucVu, maChiNhanh, hinhAnh)
+VALUES ('NVHN012', 'Nguyễn Văn An', '123456283012', '1990-01-15', N'Nam', '123 Đường Lê Lợi, Hà Nội', '0932345768', 'Quản lý', 'CNHN', ''),
+
+select * from TAIKHOAN
+-- Tự động xóa tài khoản khi xóa nhân viên
+create or alter trigger tg_DeleteTK on NhanVien
+for delete
+as
+begin
+	declare @tenDangNhap nvarchar(20), @chucVu nvarchar(50)
+	select @tenDangNhap = maNhanVien, @chucVu = chucVu from deleted
+	delete TAIKHOAN where TAIKHOAN.tenDangNhap = @tenDangNhap
+end
+
 -- Tạo TRIGGER khi thêm nhân viên thì tài khoản tự động thêm
--- Set Tên đăng nhập mặc định là mã nhân viên
 create or alter trigger trg_ThemTaiKhoan
 on NHANVIEN
 for insert
@@ -79,8 +101,18 @@ as
 begin
 	declare @taiKhoan nvarchar(20), @chucVu nvarchar(50)
 	select @taiKhoan = maNhanVien, @chucVu = chucVu from inserted
-	insert into TAIKHOAN values (@taiKhoan, '1', @chucVu, @taiKhoan)
+	insert into TAIKHOAN values (@taiKhoan, '1', @chucVu)
 end
+go
+
+-- Test
+begin tran
+	INSERT INTO CHINHANH (maChiNhanh, tenChiNhanh, diaChi)
+	VALUES ('CN001', N'Chi nhánh A', N'123 Đường A, Quận 1, TP.HCM')
+	INSERT INTO NHANVIEN (maNhanVien, hoTenNhanVien, CCCD, ngaySinh, gioiTinh, diaChi, soDienThoai, chucVu, maChiNhanh)
+	VALUES ('NV001', N'Nguyễn Văn A', '123456789012', '1990-05-15', N'Nam', N'123 Đường X, Quận Y, TP.HCM', '0123456789', N'Quản lý', 'CN001')
+	select * from TaiKhoan
+rollback
 go
 
 -- Tạo trigger Khi sửa mã nhân viên thì tài khoản cũng sẽ cập nhật theo
@@ -93,8 +125,71 @@ begin
 	set @taiKhoanMoi = (select maNhanVien from inserted)
 	set @taiKhoanCu = (select maNhanVien from deleted)
 	Update TAIKHOAN
-	set tenDangNhap = @taiKhoanMoi, maNhanVien = @taiKhoanMoi
+	set tenDangNhap = @taiKhoanMoi
 	where tenDangNhap = @taiKhoanCu
 end
+go
+
+CREATE or ALTER TRIGGER tg_ThayDoiTrangThaiHoaDon on CHITIETHOADONXE 
+for update, insert as
+BEGIN
+	DECLARE @soTienDaTra money, @maHoaDon nvarchar(20), @tongTien money
+	SELECT @soTienDaTra = ins.soTienDaTra, @maHoaDon = ins.maHoaDon FROM inserted as ins
+	SElECT @tongTien = hd.tongTien FROM HOADON as hd WHERE hd.maHoaDon = @maHoaDon
+	IF @tongTien <= @soTienDaTra
+	BEGIN 
+		UPDATE HOADON 
+		SET tinhTrang = N'Đã Thanh Toán'
+		WHERE maHoaDon = @maHoaDon
+	END
+END
+
+select * from HOADON
+select * from CHITIETHOADONXE
+
+--Trigger Kiểm tra chi nhánh không quá 2 người quản lý
+CREATE or ALTER TRIGGER tg_MotQuanLy on NHANVIEN
+AFTER UPDATE, INSERT as
+BEGIN 
+	DECLARE @maChiNhanh nvarchar(20), @chucVu nvarchar(50), @count int, @maNhanVien nvarchar(20)
+	SELECT @maChiNhanh = ins.maChiNhanh, @chucVu = ins.chucVu, @maNhanVien = ins.maNhanVien, @count = nv.soluong FROM inserted as ins, (SELECT nv.maChiNhanh, count(nv.maNhanVien) as soluong
+																						FROM NHANVIEN as nv 
+																						WHERE nv.chucVu = N'Quản lý' 
+																						GROUP BY nv.maChiNhanh) as nv
+																WHERE ins.maChiNhanh = nv.maChiNhanh
+	IF @chucVu = N'Quản lý' and @count > 1
+	BEGIN
+		PRINT N'Không thể thêm quản lý'
+		UPDATE NHANVIEN
+		SET chucVu = null
+		WHERE maNhanVien = @maNhanVien
+	END
+END
+
+select * from NHANVIEN
+
+-- Trigger khi nhập lô xe sẽ tự tạo maXe tương ứng
+CREATE or ALTER TRIGGER tg_TaoMaXe on CHITIETPHIEUNHAPXE
+AFTER INSERT as
+BEGIN
+	DECLARE @maLoXe nvarchar(20), @soLuong int, @maChiNhanh nvarchar(20)
+	SELECT @maLoXe = ins.maLoXe, @soLuong = ins.soLuong, @maChiNhanh = pn.maChiNhanh FROM inserted as ins, PHIEUNHAP as pn
+	WHERE pn.maPhieuNhap = ins.maPhieuNhap
+	WHILE @soLuong > 0
+	BEGIN
+		DECLARE @maXe nvarchar(20)
+		SET @maXe = CONCAT(@maLoXe,'_', @maChiNhanh, REPLICATE('0', 4-LEN(@soLuong)),str(@soLuong, LEN(@soLuong))) 
+		INSERT INTO XE(maXe, maLoXe)
+		Values (@maXe, @maLoXe)
+		SET @soLuong = @soLuong - 1
+	END
+END
+
+-- Test
+begin tran
+	INSERT INTO CHITIETPHIEUNHAPXE (maChiTietPhieuNhapXe, maLoXe, maPhieuNhap, giaNhap, soLuong)
+	VALUES ('CTX006', 'LOXE001', 'PN001', 1200000, 10)
+	Select * from XE
+rollback
 go
 
